0ad1b767daab37443ebfefda1549991e2eb7b99a
Guilherme Quentel Melo <gqmelo@gmail.com>
2016-12-28 20:38:49 -0200

    Add missing error check

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,10 @@
 REM Move only desired files as the downloaded package contains even Qt5 dlls
 move bin\cmake.exe %LIBRARY_BIN%\
+if errorlevel 1 exit 1
 move bin\cmcldeps.exe %LIBRARY_BIN%\
+if errorlevel 1 exit 1
 move bin\cpack.exe %LIBRARY_BIN%\
+if errorlevel 1 exit 1
 move bin\ctest.exe %LIBRARY_BIN%\
 
 if errorlevel 1 exit 1
