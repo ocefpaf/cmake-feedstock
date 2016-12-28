e45a5daad8118f644850c7599f3a50abdf3039ce
Guilherme Quentel Melo <guilherme@esss.com.br>
2016-12-28 18:24:35 -0200

    Don't include Qt5 dlls on Windows
    This was conflicting with Qt5 packages, essentially replacing
    the files and causing crashes.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,9 @@
-move bin\* %LIBRARY_BIN%\
+REM Move only desired files as the downloaded package contains even Qt5 dlls
+move bin\cmake.exe %LIBRARY_BIN%\
+move bin\cmcldeps.exe %LIBRARY_BIN%\
+move bin\cpack.exe %LIBRARY_BIN%\
+move bin\ctest.exe %LIBRARY_BIN%\
+
 if errorlevel 1 exit 1
 
 move share %LIBRARY_PREFIX%\
