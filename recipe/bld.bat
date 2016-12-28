cb2598d71de83c3472b7122135a570988d929561
Guilherme Quentel Melo <gqmelo@gmail.com>
2016-12-28 20:42:41 -0200

    Depend on vs2013_runtime on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,7 +6,12 @@ if errorlevel 1 exit 1
 move bin\cpack.exe %LIBRARY_BIN%\
 if errorlevel 1 exit 1
 move bin\ctest.exe %LIBRARY_BIN%\
+if errorlevel 1 exit 1
 
+REM This file needs to be in a separate package.
+REM Just make sure that it exists here so we can detect when cmake people
+REM decide to change the dependency.
+dir bin\msvcr120.dll
 if errorlevel 1 exit 1
 
 move share %LIBRARY_PREFIX%\
