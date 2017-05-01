9db21fc0625a72d09aaac74ac93b8948c83c2789
Michel Zou <xantares09@hotmail.com>
2017-05-01 17:59:43 +0200

    Fix ssl test

--- recipe/SSLTest.cmake
+++ recipe/SSLTest.cmake
@@ -1,6 +1,6 @@
 set(FILE_NAME "LICENSE")
 set(DOWNLOAD_URL "https://raw.githubusercontent.com/conda-forge/cmake-feedstock/master/${FILE_NAME}")
-set(EXPECTED_SHA256 "f8c925402b90f74566f71e64eea7bb9d83daac69cde71408ffd6c043be6a4991")
+set(EXPECTED_SHA256 "89593722a0290d7c22dba528a21126881eba4df1ec1be7c47380ffb58c13d5a4")
 
 file(DOWNLOAD ${DOWNLOAD_URL} ${CMAKE_CURRENT_BINARY_DIR}/${FILE_NAME}
  SHOW_PROGRESS
